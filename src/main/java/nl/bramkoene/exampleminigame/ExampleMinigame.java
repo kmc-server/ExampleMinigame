82e6fa5ddefed77a6bfc56b61fdc2efebc50067a
minestarnl <bramkoene@gmail.com>
2019-10-24 18:52:58 +0200

    init

---
+++ src/main/java/nl/bramkoene/exampleminigame/ExampleMinigame.java
@@ -0,0 +1,17 @@
+package nl.bramkoene.exampleminigame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExampleMinigame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
