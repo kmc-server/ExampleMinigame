dad6257b841814bf1a313e31d0ad4433a8085455
minestarnl <bramkoene@gmail.com>
2019-10-26 12:53:34 +0200

    Now using minigameapi v1.0.0.2

--- src/main/java/nl/bramkoene/exampleminigame/Game.java
+++ src/main/java/nl/bramkoene/exampleminigame/Game.java
@@ -3,6 +3,7 @@ package nl.bramkoene.exampleminigame;
 import nl.bramkoene.minigameapi.GameConnector;
 
 public class Game extends GameConnector {
-    public String gameName = "";
-
+    public Game() {
+        gameName = "ExampleMinigame";
+    }
 }
